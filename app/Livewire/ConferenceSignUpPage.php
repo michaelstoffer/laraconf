490a0dfcadbe17c310dcc1e4ddddd0b3cdf46297
michaelstoffer <mstoffer@michaelstoffer.com>
2025-07-23 11:41:05 -0400

    Updating postcss config and adding signup page
    Signed-off-by: michaelstoffer <mstoffer@michaelstoffer.com>

---
+++ app/Livewire/ConferenceSignUpPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ConferenceSignUpPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.conference-sign-up-page');
+    }
+}
